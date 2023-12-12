aae13775de9f4e02d7daa67540e6e00b3a0ff65b
evilfoxes <evilfoxes@mail.ru>
2023-12-12 20:23:21 +1100

    Save_5

--- task_15.py
+++ task_15.py
@@ -14,5 +14,20 @@ Input: 5 -> 5 1 6 5 9
 Output: 1 9
 """
 
-lst = list(map(int, input("Введите числа через пробел: ").split(" ")))
-print(min(lst), max(lst))
+from random import randint
+
+
+num = randint(1, 20)
+weight = randint(5, 15)
+print(weight, end=' ')
+min_weight = weight
+max_weight = weight
+
+for _ in range(num - 1):
+    weight = randint(5, 15)
+    print(weight, end=' ')
+    max_weight = max(max_weight, weight)
+    min_weight = min(min_weight, weight)
+
+print()
+print(min_weight, max_weight)
